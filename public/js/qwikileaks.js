ed84856b606c18fddf166950f8c466261a5d0d44
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-03-05 17:38:54 -0800

    grab cable data

--- public/js/qwikileaks.js
+++ public/js/qwikileaks.js
@@ -1,7 +1,19 @@
 
 function cablesearch(r) {
-  //loaded = true;
-  $.each(r.items, function(index,item) {
-    alert(item.snippet);
-  });
+  loaded = true;
+  var cable = r.items[0];
+  snippet = cable.snippet;
+  app.log(cable.title);
+  app.log(snippet);
+  var link=cable.link.replace(/.*\/(.*)\.html/,'$1');
+  $(document.body).append('<script src="http://cablesearch.org/cable/api/cable?id='+(link)+'&jsonp=cablegrab"><\/script>');
+}
+
+function cablegrab(r) {
+  abstract = r.items[0].text;
+  //abstract = abstract.replace("\n"," ");
+  //abstract = abstract.replace(/\s+/," ");
+  abstract = abstract.substring(abstract.indexOf("SUBJECT"),abstract.length);
+  app.log(abstract);
+  consume();
 }
