9816c8993d6069caeb7c693dfef50f1eca9a455e
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-03-06 20:19:59 -0800

    preparing animations

--- public/js/qwikileaks.js
+++ public/js/qwikileaks.js
@@ -27,3 +27,42 @@ function cablegrab(r) {
   app.log(abstract);
   consume();
 }
+
+// target frames per second
+const FPS = 30;
+var x = 0;
+var y = 0;
+var xDirection = 1;
+var yDirection = 1;
+var canvas = null;
+var context2D = null;
+
+function draw()
+{
+	context2D.clearRect(0, 0, canvas.width, canvas.height);
+	context2D.drawImage(image, x, y);
+	x += 1 * xDirection;
+	y += 1 * yDirection;
+	
+	if (x >= 450)
+	{
+		x = 450;
+		xDirection = -1;
+	}
+	else if (x <= 0)
+	{
+		x = 0;
+		xDirection = 1;
+	}
+	
+	if (y >= 250)
+	{
+		y = 250;
+		yDirection = -1;
+	}
+	else if (y <= 0)
+	{
+		y = 0;
+		yDirection = 1;
+	}
+}
