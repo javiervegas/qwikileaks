2562c89c90b79d1ae742ce578482bdc809260912
Javier Vegas <javiervegas@Javier-Vegass-MacBook-Pro.local>
2011-03-06 16:01:44 -0800

    mock google so they dont rate limit me

--- public/js/fqwiki.js
+++ public/js/fqwiki.js
@@ -140,6 +140,7 @@ function qwiki(q) {
 	$('#results').html('');
 	clearTimeout(timer);
 	//$(document.body).append('<script src="http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles='+(q)+'&rvprop=content&rvsection=0&callback=wikiabstract"><\/script>');
-	$(document.body).append('<script src="https://www.googleapis.com/customsearch/v1?key=AIzaSyC7_lilERhwTxuEqAaHNNk7miYaBKcP5EQ&cx=016894559323830144684:psimmjsuxr4&q='+(q)+'&callback=cablesearch"><\/script>');
+	//$(document.body).append('<script src="https://www.googleapis.com/customsearch/v1?key=AIzaSyCmmCUH9YYgjb0tVoE4PBz-zITLdueYTDY&cx=016894559323830144684:psimmjsuxr4&q='+(q)+'&callback=cablesearch"><\/script>');
+	$(document.body).append('<script src="/json/search.json"><\/script>');
 }
 
